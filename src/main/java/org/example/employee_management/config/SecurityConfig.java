237121082f2aa90dac7eec813fc94e3a8dd44197
vegpulao <dariusnokbe@gmail.com>
2026-07-07 19:56:23 +0530

    JWT dependacies added

--- src/main/java/org/example/employee_management/config/SecurityConfig.java
+++ src/main/java/org/example/employee_management/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package org.example.employee_management.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,5 +28,12 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config)
+            throws Exception {
+
+        return config.getAuthenticationManager();
     }
 }
--- src/main/java/org/example/employee_management/service/Authenticationservice.java
+++ src/main/java/org/example/employee_management/service/Authenticationservice.java
@@ -1,22 +1,28 @@
 package org.example.employee_management.service;
 
+import org.example.employee_management.dto.LoginRequest;
 import org.example.employee_management.entity.AppUser;
 import org.example.employee_management.repository.AppUserrepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Authenticationservice {
 
+    private final AuthenticationManager authenticationManager;
     private final AppUserrepository repository;
     private final PasswordEncoder passwordEncoder;
 
     // Authentication
     public Authenticationservice(AppUserrepository repository,
-                                 PasswordEncoder passwordEncoder) {
+                                 PasswordEncoder passwordEncoder,
+                                 AuthenticationManager authenticationManager) {
 
         this.repository = repository;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
     }
     // Registration
     public AppUser register(AppUser appUser) {
@@ -26,6 +32,15 @@ public class Authenticationservice {
         );
 
         return repository.save(appUser);
+    }
+    public String login(LoginRequest request){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+        return "Login Successful";
     }
 
 }
